0c72a945dcb115be56ada862a72965cb7a953605
Thomas Siskos <thomas.siskos91@gmail.com>
2022-08-27 20:24:31 +0200

    make string output of measurements purty 2

--- sensors/_general.py
+++ sensors/_general.py
@@ -24,8 +24,8 @@ class Record:
         self.value = value
 
     def __str__(self):
-        var_part = f"{self.variable}:".ljust(7)
-        val_part = f"{self.value:.2f}".rjust(7)
+        var_part = f"{self.variable}:".ljust(15)
+        val_part = f"{self.value:.2f} {self.unit}".rjust(15)
         taken_at_part = f"taken at {self.taken_at.strftime('%Y-%m-%d %H:%M:%S')}."
 
         return f"{var_part} {val_part} {taken_at_part}"
