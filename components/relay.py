4f193a4bb0a014f70c0cbbe7641975f6f2140331
Thomas Siskos <thomas.siskos91@gmail.com>
2022-11-18 19:37:05 +0100

    change relay controls - apparently LOW turns the relay ON

--- components/relay.py
+++ components/relay.py
@@ -9,11 +9,11 @@ GPIO.setup(channel, GPIO.OUT)
 
 
 def motor_on(pin):
-    GPIO.output(pin, GPIO.HIGH)  # Turn motor on
+    GPIO.output(pin, GPIO.LOW)  # Turn motor on
 
 
 def motor_off(pin):
-    GPIO.output(pin, GPIO.LOW)  # Turn motor off
+    GPIO.output(pin, GPIO.HIGH)  # Turn motor off
 
 
 if __name__ == '__main__':
