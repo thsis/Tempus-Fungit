7744a6684d2209cb2d9012382d9dda64d96c1235
Thomas Siskos <thomas.siskos91@gmail.com>
2022-11-27 11:06:32 +0100

    refactor relay code

--- components/relay.py
+++ components/relay.py
@@ -1,27 +1,60 @@
 import RPi.GPIO as GPIO
+from colorama import Fore, Style
 import time
 
-channel = 21
-
-# GPIO setup
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(channel, GPIO.OUT, initial=GPIO.HIGH)
 
 
-def motor_on(pin):
-    GPIO.output(pin, GPIO.LOW)  # Turn motor on
+class Relay:
+    def __init__(self, channel, active_low=True, initial_on=False):
+        self.channel = channel
+        self.active_low = active_low
+        self.status = initial_on
 
+        GPIO.setup(self.channel, GPIO.OUT,
+                   initial=self._get_turn_on_signal() if self.status else self._get_turn_off_signal())
 
-def motor_off(pin):
-    GPIO.output(pin, GPIO.HIGH)  # Turn motor off
+    def __del__(self):
+        GPIO.cleanup(self.channel)
+
+    def _get_turn_on_signal(self):
+        if self.active_low:
+            return GPIO.LOW
+        else:
+            return GPIO.HIGH
+
+    def _get_turn_off_signal(self):
+        if self.active_low:
+            return GPIO.HIGH
+        else:
+            return GPIO.LOW
+
+    def get_status(self):
+        if self.status:
+            msg = f"Relay Status on GPIO Pin {self.channel}: {Fore.RED}[ARMED]{Style.RESET_ALL}"
+        else:
+            msg = f"Relay Status on GPIO Pin {self.channel}: {Fore.GREEN}[DISARMED]{Style.RESET_ALL}"
+        return self.status, msg
+
+    def _set_status(self, status):
+        self.status = status
+
+    def arm(self):
+        GPIO.output(self.channel, self._get_turn_on_signal())
+        self._set_status(True)
+
+    def disarm(self):
+        GPIO.output(self.channel, self._get_turn_off_signal())
+        self._set_status(False)
 
 
 if __name__ == '__main__':
-    input("Start?:")
+    relay_1 = Relay(21)
     try:
-        motor_on(channel)
-        time.sleep(5)
-        motor_off(channel)
-        GPIO.cleanup()
+        while True:
+            relay_1.arm()
+            time.sleep(5)
+            relay_1.disarm()
     except KeyboardInterrupt:
+        relay_1.disarm()
         GPIO.cleanup()
