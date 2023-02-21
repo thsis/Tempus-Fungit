a57dbfd1d952665988a7a7e9d61e99898c964762
Thomas Siskos <thomas.siskos91@gmail.com>
2023-02-21 19:37:07 +0100

    add created_at and updated_at timestamps to tables

--- database/queries/create_tables.sql
+++ database/queries/create_tables.sql
@@ -1,12 +1,25 @@
+create or replace function update_modified_column()
+returns trigger as $$
+    begin
+        new.updated_at = now();
+        return new;
+    end;
+    $$ language 'plpgsql';
+
 create table raw_recipes (
-    created_at timestamp,
+    imported_at timestamp default now(),
+    updated_at timestamp default imported_at,
     recipe_filename varchar,
     recipe_title varchar,
     ingredients_str varchar,
     instructions_str varchar
 );
 
+create trigger update_raw_recipes_update_timestamp before update on raw_recipes for each row execute procedure update_modified_column();
+
 create table raw_sensor_readings (
+    imported_at timestamp default now(),
+    updated_at timestamp default imported_at,
     site varchar,
     taken_at date,
     sensor varchar,
@@ -15,7 +28,11 @@ create table raw_sensor_readings (
     value float
 );
 
+create trigger update_raw_sensor_readings_update_timestamp before update on raw_sensor_readings for each row execute procedure update_modified_column();
+
 create table raw_experiments (
+    created_at timestamp default now(),
+    updated_at timestamp default created_at,
     date_observed date,
     date_created date,
     date_terminated date,
@@ -30,10 +47,16 @@ create table raw_experiments (
     note varchar
 );
 
+create trigger update_raw_experiments_update_timestamp before update on raw_experiments for each row execute procedure update_modified_column();
+
 create table raw_yields (
-  experiment_create_at date,
-  experiment_replicate_id int,
-  date_harvested date,
-  fruiting_body_id int,
-  fruiting_body_weight float
+    created_at timestamp default now(),
+    updated_at timestamp default created_at,
+    experiment_created_at date,
+    experiment_replicate_id int,
+    date_harvested date,
+    fruiting_body_id int,
+    fruiting_body_weight float
 );
+
+create trigger update_raw_yields_update_timestamp before update on raw_yields for each row execute procedure update_modified_column();
